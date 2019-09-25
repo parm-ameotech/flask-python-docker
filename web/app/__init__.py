8a0075e865ff8e294af20c79093a8bd68b47c3a8
Bledi Gegaj <bledigegaj@gmail.com>
2019-09-25 16:15:58 +0300

    variable edit

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -5,10 +5,12 @@ import os
 # add those later
 DBHOST = os.environ.get("DBHOST")
 DBPORT = os.environ.get("DBPORT")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 DBNAME = "postgres" #for now
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@{}:{}/{}".format(DBHOST, DBPORT, DBNAME)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://{}:{}@{}:{}/{}".format(POSTGRES_USER, POSTGRES_PASSWORD, DBHOST, DBPORT, DBNAME)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 print(db.session.execute('SELECT 1'))
