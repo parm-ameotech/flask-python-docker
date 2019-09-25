f816c3173464d0ebab0f405a9032cffe249956b1
Bledi Gegaj <bledigegaj@gmail.com>
2019-09-25 15:58:54 +0300

    docker-run

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+
+# add those later
+DBHOST = os.environ.get("DBHOST")
+DBPORT = os.environ.get("DBPORT")
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@db:8292/postgres"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@postgres_distribution/postgres"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 print(db.session.execute('SELECT 1'))
+
 from . import routes
