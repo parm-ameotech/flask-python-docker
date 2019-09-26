360bf47af0db34fa360545a7db2f548ac17d83a4
Eslam Samir <eslamsamir232@gmail.com>
2019-09-26 15:35:41 +0200

    add daemon process

--- web/server.py
+++ web/server.py
@@ -1,3 +1,6 @@
+from multiprocessing import Process
+import threading
+
 from flask import Flask
 from app import app
 
@@ -9,4 +12,19 @@ else:
   app.debug = False
 
 
-app.run(host="0.0.0.0")
+def daemon_process():
+    print("daemon is working")
+    threading.Timer(1, daemon_process).start()
+
+def flask_process():
+    app.run(host="0.0.0.0")
+
+
+if __name__ == '__main__':
+  p1 = Process(target=daemon_process)
+  p1.start()
+  p2 = Process(target=flask_process)
+  p2.start()
+
+  p1.join()
+  p2.join()
