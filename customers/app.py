2378345a1795f4a3bd5da7f4722994962d1f3319
gurnoor <gurnoor.ameotech1@gmail.com>
2019-11-06 12:55:12 +0530

    Setup rest api using flask and docker

--- customers/app.py
+++ customers/app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("index.html")
-
-
+	data = {
+		'name': "index"
+	}
+	return render_template("index.html", data=data)
 
 app.run(debug=True,host="0.0.0.0",port=5001)
